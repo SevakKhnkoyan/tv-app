ec2bd6bcb5638930d826931af74c042ff1a03d57
Sevak Khnkoyan <sevak.khnkoyan@digitain.com>
2025-08-09 20:53:08 +0400

    sidebar minor 2

--- src/components/SidebarMenu.tsx
+++ src/components/SidebarMenu.tsx
@@ -33,6 +33,7 @@ export default function SidebarMenu({selectedMenuItem, onSelectMenuItem}: Sideba
   return (
     <motion.aside
       className="
+        z-40
         group/sidebar
         fixed left-0 top-0 h-full
         bg-black text-white
@@ -45,7 +46,7 @@ export default function SidebarMenu({selectedMenuItem, onSelectMenuItem}: Sideba
       initial={{ width: 130 }}
       whileHover={{
         width: 350,
-        background: 'linear-gradient(to right, rgba(0,0,0,1) 130px, rgba(0,0,0,0.8) 100%)'
+        background: 'linear-gradient(to right, rgba(0,0,0,1) 130px, rgba(0,0,0,0.2) 100%)'
       }}
       transition={{ duration: 0.3 }}
     >
